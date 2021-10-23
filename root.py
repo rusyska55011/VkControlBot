8034643e15b1b441b8c3cf4cde12131ee63053df
rusyska55011 <ruslan.nikandrov2015@yandex.ru>
2021-10-23 15:03:01 +0300

    Added new class for vk_api

--- root.py
+++ root.py
@@ -4,6 +4,7 @@ from tkinter import *
 
 from time import sleep
 from random import randint
+from threading import Thread
 
 import vk_api
 from vk_api.utils import get_random_id
@@ -377,3 +378,37 @@ class File:
                 arg = arg[:-1]
             data.append(arg)
         return tuple(data)
+
+
+class AddUsersChecker:
+    def __init__(self, vk_bot, message_for_added_users: str, friends_list: list):
+        self.message_for_added_users = message_for_added_users
+        self.friends_list = friends_list
+        self.vk_bot = vk_bot
+
+    def start(self):
+        userschecker_thread = Thread(target=self.__process)
+        userschecker_thread.start()
+
+    def __process(self):
+        while True:
+            get_friends = self.vk_bot.get_friends_list()
+            check = self.check_friends(self.friends_list, get_friends)
+
+            if check:
+                print('Обнаружены новые друзья! ID:', check)
+                for vk_id in check:
+                    self.vk_bot.send_message(vk_id, self.message_for_added_users)
+                # Чтобы бот забывал друзей, которые удались из списка друзей,
+                # можно передвинуть переменную за блок if
+            self.friends_list = get_friends
+
+            sleep(randint(5, 15))
+
+    @staticmethod
+    def check_friends(actual_friends_list: list, new_friends_list: list) -> list:
+        if actual_friends_list != new_friends_list:
+            new_friends = [item for item in new_friends_list if item not in actual_friends_list]
+            return new_friends
+        else:
+            return list()
